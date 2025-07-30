df589f1c80fc25a57b341dbc24884577b23a96ed
John Radebe <0307295707086@tpc.local>
2025-07-30 11:07:05 +0200

    created automation 'Project-Navigator'

---
+++ Project-Navigator.sh
@@ -0,0 +1,3 @@
+touch summary.txt
+echo "The project's purpose is to manage the developement team effectively, and to also ensure that everyone knows what is expected from them. My personal details are, my name and age, which is John Radebe and my age is 22." > Summary.txt
+echo "Summary created, good job"
